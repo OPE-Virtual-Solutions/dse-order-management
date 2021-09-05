4c34fb349c8ee07ee9712e36e3e220839cdc9c67
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-05 13:08:05 -0300

    feat: adicionado layout da página de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,39 @@
 import styles from "./styles.module.css";
 
+import TextField from '@material-ui/core/TextField';
+
 function Login() {
+    document.title = "DSE - Autenticação de Usuário"
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className={ styles.loginContainer }>
+            <div className={ styles.loginHeaderContainer }>
+                <img src="logo192.png" alt="Logo da Virtual Solutions" />
+                <h4>Delivery System Express</h4>
+                <span>Sistema de gerenciamento de pedidos</span>
+            </div>
+
+            <h6>Autenticação de Usuário</h6>
+
+            <form className={ styles.loginFormContainer }>
+                <div className="mb-3">
+                    <TextField label="E-mail" fullWidth variant="outlined" />
+                </div>
+                <div className="mb-3">
+                    <TextField type="password" label="Senha" fullWidth variant="outlined" />
+                </div>
+
+                <div className="form-group d-flex align-items-center justify-content-between">
+                    <div className="form-check">
+                        <input type="checkbox" value="" className="form-check-input" id="flexCheckbox" />
+                        <label htmlFor="flexCheckbox">Lembrar-me</label>
+                    </div>
+
+                    <button type="submit" className="btn-solid">
+                        Entrar
+                    </button>
+                </div>
+            </form>
         </div>
     )
 };
