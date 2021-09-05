b5d93aa4501ea127d3867dea31edaa356185f989
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-05 18:33:07 -0300

    feat: adicionado componente de navlink
    Responsável pelos botões presentes na barra lateral de navegação.

---
+++ src/components/NavLink/index.tsx
@@ -0,0 +1,49 @@
+import { ReactNode } from "react";
+
+import { NavLink as Link } from "react-router-dom";
+
+import Tooltip from "@material-ui/core/Tooltip";
+import { withStyles } from "@material-ui/styles";
+
+const NavTooltip = withStyles((theme) => ({
+    tooltip: {
+        backgroundColor: "#FFF",
+        color: "var(--text)",
+        maxWidth: 220,
+        fontSize: theme.typography.pxToRem(13),
+        fontWeight: 600,
+        border: "1px solid var(--divider)"
+    }
+}))(Tooltip);
+
+type Props = {
+    title: string;
+    path: string;
+    children: ReactNode;
+}
+
+function NavLink({
+    title,
+    path,
+    children
+}: Props) {
+    return (
+        <NavTooltip title={title} placement="right">
+            <div>
+                <Link 
+                    exact 
+                    to={path} 
+                    className="navLink"
+                    activeStyle={{
+                        backgroundColor: "var(--secondary)",
+                        color: "var(--text)"
+                    }}
+                >
+                    { children }
+                </Link>
+            </div>
+        </NavTooltip>
+    )
+};
+
+export { NavLink };
