20951ab36bd010ef41576c8d283a8ef963921aec
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-09 18:04:09 -0300

    feat: adicionado layout da página de gerenciamento de ingredientes

--- src/pages/Ingredients/IngredientDashboard/index.tsx
+++ src/pages/Ingredients/IngredientDashboard/index.tsx
@@ -1,14 +1,51 @@
-import { SideMenu } from "components/SideMenu";
+import { InputAdornment, TextField } from "@material-ui/core";
+import { Button } from "components/forms/Button";
+
+import { FaPlus, FaSearch } from "react-icons/fa";
+
+import { Dashboard } from "templates/Dashboard";
+
+import styles from "./styles.module.css";
 
 function IngredientDashboard() {
     document.title = "DSE - Gerenciamento de Ingredientes"
 
     return (
-        <div className="dashboardContainer">
-            <SideMenu />
+        <Dashboard>
+            <div className={ styles.ingredientDashboardContainer }>
+                <header>
+                    <h4>Ingredientes</h4>
 
-            <h1>Ingredientes</h1>
-        </div>
+                    <div>
+                        <TextField 
+                            type="text" 
+                            className="me-1" 
+                            label="Pesquisar ingrediente" 
+                            size="small"
+                            variant="outlined"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <FaSearch color="var(--divider)" />
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+
+                        <Button 
+                            outline 
+                            icon={<FaPlus size={14} />} 
+                            text="Adicionar" 
+                            className="me-1" 
+                        />
+                    </div>
+                </header>
+
+                <div className={ styles.ingredientContentContainer }>
+                    <span>abc</span>
+                </div>
+            </div>
+        </Dashboard>
     )
 };
 
