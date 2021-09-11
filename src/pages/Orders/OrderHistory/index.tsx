8784607ca940f762b59637045fa7857feacf92b9
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-11 10:43:51 -0300

    feat: adicionado arquivo da rota de histórico de pedidos

---
+++ src/pages/Orders/OrderHistory/index.tsx
@@ -0,0 +1,12 @@
+import { Dashboard } from "templates/Dashboard";
+import styles from "./styles.module.css";
+
+function OrderHistory() {
+    return (
+        <Dashboard>
+            <h1>Histórico de pedidos</h1>
+        </Dashboard>
+    )
+};
+
+export { OrderHistory };
