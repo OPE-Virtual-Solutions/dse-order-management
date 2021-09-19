11d59c88682800a556b54e5b14c124427450fbf5
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-19 17:30:41 -0300

    feat: adicionado layout do componente de cartão de produto da loja/atendimento
    Utilizado para o atendimento/criação de um novo pedido, igual os cartões de produtos utilizados em
    e-commerces.

---
+++ src/components/cards/ProductShopCard/index.tsx
@@ -0,0 +1,54 @@
+import styles from "./styles.module.css";
+
+import { IProduto } from "interfaces";
+
+import { FaShoppingBag } from "react-icons/fa";
+
+import { TextField } from "@material-ui/core";
+import { Button } from "components/forms/Button";
+
+type Props = {
+    product: IProduto;
+}
+
+function ProductShopCard({ product }: Props) {
+    return (
+        <div className={ styles.cardContainer }>
+            <main>
+                <div className={ styles.placeholderPic }></div>
+
+                <div className={ styles.productInfoContainer }>
+                    <h5>{ product.nome }</h5>
+                    <h5>R${ product.preco }</h5>
+
+                    <p>
+                        { product.ingredientes.map((ingredient, index) => (
+                            index === product.ingredientes.length - 1 ? ingredient.nome : ingredient.nome + ", "
+                        ))}
+                    </p>
+                </div>
+            </main>
+
+            <footer>
+                <form>
+                    <TextField 
+                        name="inputQnt"
+                        type="number" 
+                        variant="outlined" 
+                        size="small" 
+                        label="Quantidade"
+                        defaultValue={0}
+                    />
+
+                    <Button 
+                        text="Adicionar ao carrinho"
+                        icon={<FaShoppingBag />}
+                        transparent
+                    />
+                </form>
+            </footer>
+        </div>
+    )
+};
+
+export { ProductShopCard };
