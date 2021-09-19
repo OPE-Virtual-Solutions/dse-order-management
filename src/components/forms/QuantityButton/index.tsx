5ba1b9e80e2a8c75793b3616815a2f4b0d8411ad
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-19 20:12:21 -0300

    feat: adicionado componente de botão de controle de quantidade

---
+++ src/components/forms/QuantityButton/index.tsx
@@ -0,0 +1,34 @@
+import { MouseEventHandler } from "react";
+
+import { 
+    FaChevronUp, 
+    FaChevronDown 
+} from "react-icons/fa";
+
+import styles from "./QuantityButton.module.css";
+
+type Props = {
+    onSum: MouseEventHandler<HTMLButtonElement>;
+    onSubtract: MouseEventHandler<HTMLButtonElement>;
+    quantity: number;
+}
+
+function QuantityButton({
+    onSum, 
+    onSubtract,
+    quantity
+}: Props) {
+    return (
+        <div className={ styles.buttonContainer }>
+            <button onClick={onSubtract}>
+                <FaChevronDown size={12} />
+            </button>
+            <span>{ quantity }</span>
+            <button onClick={onSum}>
+                <FaChevronUp size={12}/>
+            </button>
+        </div>
+    )
+}
+
+export { QuantityButton };
