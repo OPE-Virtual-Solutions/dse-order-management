d5663dbf745d3a4cf3d46b46bcce54eca7c36ad2
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-19 17:35:25 -0300

    feat: adicionado escopo de layout da página de atendimento

--- src/pages/Orders/OrderDashboard/index.tsx
+++ src/pages/Orders/OrderDashboard/index.tsx
@@ -1,11 +1,59 @@
+import { useState } from "react";
+
 import { Dashboard } from "templates/Dashboard";
+
+import styles from "./styles.module.css";
+
+import { 
+    FaGripLines, 
+    FaBorderAll 
+} from "react-icons/fa";
+
+import { Tooltip } from "components/display/Tooltip";
+import { ProductShopCard } from "components/cards/ProductShopCard";
+
+import { products } from "pages/Products/ProductDashboard/data";
+import { Button } from "components/forms/Button";
 
 function OrderDashboard() {
     document.title = "DSE - Pedidos"
 
+    const [isGridList, setIsGridList] = useState<boolean>(true);
+
+    function handleListType() {
+        setIsGridList(!isGridList);
+    }
+
     return (
         <Dashboard>
-            <h1>Pedidos</h1>        
+            <div className={styles.orderDashboardContainer}>
+                {isGridList && (
+                    <div className={styles.productListContainer}>
+                        {products.map((produto) => (
+                            <ProductShopCard product={produto} />   
+                        ))}
+                    </div>
+                )}
+
+                {!isGridList && (
+                    <span>TODO tabela para atendimento</span>
+                )}
+                
+                <div className={styles.gridButton}>
+                    <Tooltip 
+                        title={`Visualização em ${isGridList ? "cartões" : "tabela"}`} 
+                        placement="left"
+                    >
+                        <Button 
+                            onClick={() => { handleListType() }}
+                            icon={
+                                isGridList ? <FaBorderAll /> : <FaGripLines />
+                            }
+                            transparent
+                        />
+                    </Tooltip>
+                </div>
+            </div>
         </Dashboard>
     )
 };
