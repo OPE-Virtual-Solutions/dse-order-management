ddc83b64988956c3a190b13dc896f5aba135ee1a
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-22 18:55:14 -0300

    feat: adicionado novos produtos placeholder

--- src/pages/Products/ProductDashboard/data.tsx
+++ src/pages/Products/ProductDashboard/data.tsx
@@ -19,11 +19,6 @@ const categories: ICategoria[] = [
         id: 3,
         nome: "Porções",
         ativo: true,
-    },
-    {
-        id: 4,
-        nome: "Desabilitado",
-        ativo: false,
     }
 ];
 
@@ -73,11 +68,55 @@ const products: IProduto[] = [
         quantidade: 10,
         ingredientes: [
             ingredients[0],
-            ingredients[1],
-            ingredients[2]
+            ingredients[1]
         ],
         ativo: true,
     },
+    {
+        id: 3,
+        nome: "Coca-Cola",
+        descricao: "Latinha de 250ml",
+        preco: 5.00,
+        categoria: categories[1],
+        quantidade: 51,
+        ingredientes: [],
+        ativo: true,
+    },
+    {
+        id: 4,
+        nome: "Guaraná Antártica",
+        descricao: "Latinha de 400ml",
+        preco: 5.00,
+        categoria: categories[1],
+        quantidade: 65,
+        ingredientes: [],
+        ativo: true
+    },
+    {
+        id: 5,
+        nome: "X-Bacon",
+        descricao: "Um delicioso sanduiche de bacon",
+        preco: 19.99,
+        categoria: categories[0],
+        quantidade: 43,
+        ingredientes: [
+            ingredients[0],
+            ingredients[1],
+            ingredients[2],
+            ingredients[3]
+        ],
+        ativo: true
+    },
+    {
+        id: 6,
+        nome: "Batata-frita",
+        descricao: "Uma porção deliciosa de batata-frita",
+        preco: 6.99,
+        categoria: categories[2],
+        quantidade: 78,
+        ingredientes: [],
+        ativo: true,
+    }
 ];
 
 export { categories, products, ingredients };
