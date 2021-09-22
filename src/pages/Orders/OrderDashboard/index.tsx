c2b11350ad874a812f096f09c20f4f66830727ba
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-22 18:59:53 -0300

    feat: adicionado header e agrupamento de produtos por categoria utilizando TabBar

--- src/pages/Orders/OrderDashboard/index.tsx
+++ src/pages/Orders/OrderDashboard/index.tsx
@@ -6,19 +6,24 @@ import styles from "./styles.module.css";
 
 import { 
     FaGripLines, 
-    FaBorderAll 
+    FaBorderAll, 
+    FaSearch
 } from "react-icons/fa";
+import { InputAdornment, TextField } from "@material-ui/core";
 
 import { Tooltip } from "components/display/Tooltip";
 import { ProductShopCard } from "components/cards/ProductShopCard";
-
-import { products } from "pages/Products/ProductDashboard/data";
 import { Button } from "components/forms/Button";
+import { TabBar } from "components/display/TabBar";
+
+import { categories, products } from "pages/Products/ProductDashboard/data";
 
 function OrderDashboard() {
     document.title = "DSE - Pedidos"
 
     const [isGridList, setIsGridList] = useState<boolean>(true);
+
+    const [selectedCategory, setSelectedCategory] = useState<number>(0);
 
     function handleListType() {
         setIsGridList(!isGridList);
@@ -27,32 +32,62 @@ function OrderDashboard() {
     return (
         <Dashboard showCart={true}>
             <div className={styles.orderDashboardContainer}>
-                {isGridList && (
-                    <div className={styles.productListContainer}>
-                        {products.map((produto) => (
-                            <ProductShopCard product={produto} />   
-                        ))}
-                    </div>
-                )}
 
-                {!isGridList && (
-                    <span>TODO tabela para atendimento</span>
-                )}
-                
-                <div className={styles.gridButton}>
-                    <Tooltip 
-                        title={`Visualização em ${isGridList ? "cartões" : "tabela"}`} 
-                        placement="left"
-                    >
-                        <Button 
-                            onClick={() => { handleListType() }}
-                            icon={
-                                isGridList ? <FaBorderAll /> : <FaGripLines />
-                            }
-                            transparent
+                <header>
+                    <div>
+                        <h4>Atendimento ao Cliente</h4>
+                        
+                        <TextField 
+                            type="text" 
+                            className="me-1" 
+                            label="Pesquisar produto" 
+                            size="small"
+                            variant="outlined"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <FaSearch color="var(--divider)" />
+                                    </InputAdornment>
+                                )
+                            }}
                         />
-                    </Tooltip>
-                </div>
+                    </div>
+
+                    <TabBar 
+                        selectedTab={selectedCategory}
+                        setSelectedTab={setSelectedCategory}
+                        labelList={categories.map((category) => category.nome)}
+                    />
+                </header>
+
+                <main>
+                    {isGridList && (
+                        <div className={styles.productListContainer}>
+                            { products.map((produto) => 
+                                produto.categoria.nome === categories[selectedCategory].nome && <ProductShopCard product={produto} />
+                            )}
+                        </div>
+                    )}
+
+                    {!isGridList && (
+                        <span>TODO tabela para atendimento</span>
+                    )}
+                    
+                    <div className={styles.gridButton}>
+                        <Tooltip 
+                            title={`Visualização em ${isGridList ? "cartões" : "tabela"}`} 
+                            placement="left"
+                        >
+                            <Button 
+                                onClick={() => { handleListType() }}
+                                icon={
+                                    isGridList ? <FaBorderAll /> : <FaGripLines />
+                                }
+                                transparent
+                            />
+                        </Tooltip>
+                    </div>
+                </main>
             </div>
         </Dashboard>
     )
