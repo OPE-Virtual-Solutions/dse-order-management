a281190d80a4184580bfc8636bc276edcbe00ef0
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-21 21:49:59 -0300

    feat: adicionado arquivo de componente de configuração de pedido

---
+++ src/pages/Orders/OrderConfigurationForm/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./OrderConfiguration.module.css";
+
+function OrderConfigurationForm() {
+    return (
+        <div className={styles.orderConfigurationContainer}>
+        </div>
+    )
+}
+
+export { OrderConfigurationForm };
