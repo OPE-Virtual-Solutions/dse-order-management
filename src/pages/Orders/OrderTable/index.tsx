35ca2870c8aed7752f37b65d3e6104dbeb22b95f
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-10-01 17:59:17 -0300

    feat: adicionado componente tabela para visualização de produtos do atendimento
    Utilizado no fluxo de atendimento para visualização em tabela dos produtos.

---
+++ src/pages/Orders/OrderTable/index.tsx
@@ -0,0 +1,95 @@
+import styles from "./OrderTable.module.css";
+
+import { currencyFormat } from "utils/currencyFormat";
+
+import { FaShoppingBag } from "react-icons/fa";
+
+import { 
+    Table, 
+    TableContainer,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    TextField 
+} from "@material-ui/core";
+import { Button } from "components/forms/Button";
+import { Tooltip } from "components/display/Tooltip";
+
+import { IProduto } from "interfaces";
+
+type Props = {
+    headers: string[];
+    products: IProduto[];
+    selectedCategory: string;
+}
+
+function OrderTable({
+    headers,
+    products,
+    selectedCategory
+}: Props) {
+
+    function renderTableRow(product: IProduto) {
+        return (
+            <TableRow className={ styles.tableRow }>
+                <TableCell>
+                    <div className={ styles.imgPlaceholder } />
+                </TableCell>
+                <TableCell >
+                    { product.nome }
+                </TableCell>
+                <TableCell>
+                    R${ currencyFormat(product.preco) }
+                </TableCell>
+                <TableCell>
+                    { product.ingredientes.length !== 0 ? (
+                        product.ingredientes.map((ingredient, index) => (
+                            index === product.ingredientes.length - 1 ? ingredient.nome : ingredient.nome + ", "
+                        ))) :
+                        product.descricao 
+                    }
+                </TableCell>
+                <TableCell style={{ maxWidth: 100 }}>
+                    <TextField 
+                        type="number" 
+                        defaultValue={0} 
+                        variant="outlined" 
+                        size="small" 
+                    />
+                </TableCell>
+                <TableCell>
+                    <Tooltip title="Adicionar ao carrinho" placement="right">
+                        <Button transparent icon={<FaShoppingBag />}/>
+                    </Tooltip>
+                </TableCell>
+            </TableRow>
+        )
+    }
+
+    return (
+        <div>
+            <TableContainer className={ styles.table }>
+                <Table>
+                    <TableHead className={ styles.tableHeader }>
+                        <TableRow>
+                            { headers.map((header, index) => (
+                                <TableCell key={index}>
+                                    { header }
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody className={ styles.tableBody }>
+                        { products.map((product, index) => 
+                            product.categoria.nome === selectedCategory && renderTableRow(product)
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+
+};
+
+export { OrderTable };
