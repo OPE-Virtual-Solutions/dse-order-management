f7ec0085bddd4c73a64eaaac6fe19b2ff4f5912f
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-10-04 17:47:56 -0300

    feat: adicionado provider para utilização do context de pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import { materialTheme } from './utils/materialUiTheme';
 
 import { Routes } from './routes';
 
+import { OrderProvider } from 'contexts/OrderContext/OrderContext';
+
 function App() {
   return (
     <ThemeProvider theme={materialTheme}>
       <div className="App">
-        <Routes />
+        <OrderProvider>
+          <Routes />
+        </OrderProvider>
       </div>
     </ThemeProvider>
   );
