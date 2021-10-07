830153fbf734ff5bd24904045d3692983db3c79c
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-10-07 18:46:08 -0300

    feat: sincronização do passo de conclusão de pedido ao novo fluxo da context de pedidos

--- src/pages/Orders/OrderConfigurationForm/index.tsx
+++ src/pages/Orders/OrderConfigurationForm/index.tsx
@@ -1,13 +1,41 @@
+import { useContext } from "react";
 import styles from "./OrderConfiguration.module.css";
 
 import {
     FaHome,
     FaBox,
-    FaMoneyBill
+    FaHamburger,
+    FaMoneyBill,
+    FaWifi
 } from "react-icons/fa"
 import { Button } from "components/forms/Button";
 
-function OrderConfigurationForm() {
+import { OrderContext } from "contexts/OrderContext/OrderContext";
+import { currencyFormat } from "utils/currencyFormat";
+
+function OrderConclusion() {
+    const { summary, registerOrder } = useContext(OrderContext);
+    
+    function showPaymentMethod(paymentMethod: "money" | "debit" | "credit"): string {
+        const paymentMethods = {
+            money: "dinheiro",
+            credit: "cartão de crédito",
+            debit: "cartão de débito"
+        };
+
+        return paymentMethods[paymentMethod];
+    };
+
+    function calculateChangePayment(): string {
+        const changeValue = summary.valor_pago - summary.valor_total;
+
+        if (changeValue === 0) {
+            return "--";
+        } else {
+            return currencyFormat(changeValue)
+        }
+    };
+
     return (
         <div className={styles.orderConfigurationContainer}>
             <div className={ styles.configurationColumn }>
@@ -18,14 +46,29 @@ function OrderConfigurationForm() {
                 <hr />
 
                 <div>
-                    <div className={ styles.orderLocationCard }>
-                        <span>Efetuado presencialmente</span>
-                        <FaHome />
-                    </div>
-                    <div className={ styles.orderConsumeCard }>
-                        <span>Para viagem</span>
-                        <FaBox color="var(--secondary)" />
-                    </div>
+                    { summary.atendimento_presencial ? (
+                        <div className={ styles.orderLocationCard }>
+                            <span>Efetuado presencialmente</span>
+                            <FaHome />
+                        </div>
+                    ) : (
+                        <div className={ styles.orderLocationCard }>
+                            <span>Efetuado a distância</span>
+                            <FaWifi />
+                        </div>
+                    )}
+
+                    { summary.tipo_consumo === "local" ? (
+                        <div className={ styles.orderConsumeCard }>
+                            <span>Para comer</span>
+                            <FaHamburger color="var(--secondary)" />
+                        </div>
+                    ) : (
+                        <div className={ styles.orderConsumeCard }>
+                            <span>Para viagem</span>
+                            <FaBox color="var(--secondary)" />
+                        </div>
+                    )}
                 </div>
             </div>
             <div className={ styles.configurationColumn }>
@@ -36,27 +79,37 @@ function OrderConfigurationForm() {
                 <hr />
 
                 <div>
-                    <div className={ styles.orderPaymentInfoCard }>
-                        <span>Pagamento em dinheiro</span>
-                        <FaMoneyBill />
-                    </div>
+                    { summary.metodo_pagamento && (
+                        <div className={ styles.orderPaymentInfoCard }>
+                            <span>Pagamento em { showPaymentMethod(summary.metodo_pagamento) }</span>
+                            <FaMoneyBill />
+                        </div>
+                    )}
                     
                     <div className={ styles.orderPaymentInfoCard }>
                         <span>Valor a pagar</span>
-                        <span>R$19.00</span>
+                        <span>R${ currencyFormat(summary.valor_total) }</span>
                     </div>
 
-                    <div className={ styles.orderPaymentInfoCard }>
-                        <span>Valor pago</span>
-                        <span>R$19.00</span>
-                    </div>
+                    { summary.metodo_pagamento === "money" && (
+                        <div>
+                            <div className={ styles.orderPaymentInfoCard }>
+                                <span>Valor pago</span>
+                                <span>R${ 
+                                    summary.valor_pago ? currencyFormat(summary.valor_pago) :
+                                    currencyFormat(summary.valor_total)
+                                }</span>
+                            </div>
 
-                    <div className={ styles.orderPaymentInfoCard }>
-                        <span>Troco</span>
-                        <span>R$--</span>
-                    </div>
+                            <div className={ styles.orderPaymentInfoCard }>
+                                <span>Troco</span>
+                                <span>R${ calculateChangePayment() }</span>
+                            </div>
+                        </div>
+                    )}
 
                     <Button 
+                        onClick={() => { registerOrder() }}
                         className="w-100"
                         text="Efetuar pedido"
                     />
@@ -66,4 +119,4 @@ function OrderConfigurationForm() {
     )
 }
 
-export { OrderConfigurationForm };
+export { OrderConclusion };
