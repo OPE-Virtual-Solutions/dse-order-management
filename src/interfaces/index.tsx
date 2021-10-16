6ccbe9eefdc0c6fa8d547bca7eeb37a73156eb60
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-10-16 11:37:57 -0300

    feat: adicionado exportação das novas tipagems

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -4,10 +4,32 @@ import { IIngrediente } from "./IIngrediente";
 import { IItemPedido } from "./ItemPedido";
 import { IPedido } from "./Pedido";
 
+import { CartProduct, CartProductPT } from "./CartProduct";
+import { Category, CategoryPT } from "./Category";
+import { Order, OrderPT } from "./Order";
+import { Product, ProductPost, ProductPostPT, ProductPT, EmptyProduct } from "./Product";
+import { Ingredient, IngredientPT } from "./Ingredient";
+
 export type { 
     ICategoria,
     IProduto,
     IIngrediente,
     IItemPedido,
-    IPedido
+    IPedido,
+    CartProduct,
+    CartProductPT,
+    Category,
+    CategoryPT,
+    Order,
+    OrderPT,
+    Product,
+    ProductPost,
+    ProductPostPT,
+    ProductPT,
+    Ingredient,
+    IngredientPT,
 };
+
+export {
+    EmptyProduct
+}
