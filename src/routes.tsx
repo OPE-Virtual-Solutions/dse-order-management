d8e19307ccd7e15c53578aa69ea95efbe566f9df
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-10-17 12:32:41 -0300

    feat(routes): adicionado restrição de rota no fluxo de registro de pedido

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,11 +10,15 @@ import { ProductDashboard } from "pages/Products/ProductDashboard";
 import { IngredientDashboard } from "pages/Ingredients/IngredientDashboard";
 import { OrderRegister } from "pages/Orders/OrderRegister";
 
+import { OrderRoute } from "components/routes/OrderRoute";
+
 function Routes() {
     return (
         <BrowserRouter>
             <Route component={ OrderHistory } path="/order-history" />
-            <Route component={ OrderRegister } path="/order" />
+
+            <OrderRoute component={ OrderRegister } path="/order" />
+
             <Route component={ OrderDashboard } path="/order-dashboard" />
             <Route component={ IngredientDashboard } path="/ingredients" />
             <Route component={ ProductDashboard } path="/products" />
