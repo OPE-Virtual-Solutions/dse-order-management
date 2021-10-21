d97567736f24cf800c02d722715151d1c807b566
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-10-21 18:47:16 -0300

    feat(services): adicionado métodos para listagem e edição de pedidos

--- src/services/order.service.tsx
+++ src/services/order.service.tsx
@@ -2,26 +2,56 @@ import { api } from "api";
 
 import { 
     Order,
+    OrderPT,
     OrderPostPT
 } from "interfaces";
 
 const ENDPOINT = "/pedidos/";
 
 class _OrderService {
+
+    private translateListResponse(response: OrderPT[]) {
+        return response.map((pedido: OrderPT) => {
+            return new Order(pedido);
+        });
+    }
+
+    async list() {
+        const response = await api.get(ENDPOINT);
+
+        let list: Order [] = [];
+        if (response.status) {
+            console.log("~ lista de pedidos:", response.data);
+            list = this.translateListResponse(response.data);
+            console.log("~ lista de pedidos convertida:", list);
+        };
+
+        return list;
+    };
+
     async create(order: Order) {
         const pedido: OrderPostPT = {
             codigo_pedido: order.order_code,
-            status: "em_espera",
+            status: "aguardando",
             atendimento_presencial: order.is_local_order,
             valor_total: order.total_price,
             metodo_pagamento: order.payment_method,
-            criado_em: new Date()
+            criado_em: new Date(),
+            tipo_pedido: order.order_type || "pra_consumir"
         };
 
         const response = await api.post(ENDPOINT, pedido);
 
         return response;
-    }
+    };
+
+    async update(id: number, order: Order) {
+        const pedido = new OrderPT(order);
+
+        const response = await api.patch(ENDPOINT + `${id}/`, pedido);
+    
+        return response;
+    };
 };
 
 export const OrderService = new _OrderService();
