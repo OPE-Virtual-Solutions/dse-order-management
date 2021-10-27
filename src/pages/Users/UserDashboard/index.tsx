ea769d71399c9d78fcd7cb64d8aba5d3ecdc2811
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-10-27 18:28:35 -0300

    feat: adicionado arquivos da dashboard de manipulação de usuários

---
+++ src/pages/Users/UserDashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Dashboard } from "templates/Dashboard";
+
+function UserDashboard() {
+    return (
+        <Dashboard>
+            <span>Dashboard para listagem de usuário</span>
+        </Dashboard>
+    )
+};
+
+export { UserDashboard };
