d78e1df6d440e5fa27f6381285263bf675c3fcab
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-11-03 19:28:44 -0300

    feat(order): adicionado componente para visualização de dados de um produto do pedido

---
+++ src/components/cases/Orders/OrderProductInfo/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./OrderProductInfo.module.css";
+
+import { CartProduct } from "interfaces";
+
+type Props = {
+    cartProduct: CartProduct;
+}
+
+function OrderProductInfo({ cartProduct }: Props) {
+    const product = cartProduct.product;
+
+    return (
+        <div className={styles.container}>
+            <p>
+                { product.name } ● <span>Qtd.</span> { cartProduct.quantity } 
+            </p>
+
+            <div>
+                {product.ingredients.length !== 0 && (
+                    <span>
+                        Ver ingredientes
+                    </span>
+                )}
+            </div>
+        </div>
+    )
+};
+
+export { OrderProductInfo };
