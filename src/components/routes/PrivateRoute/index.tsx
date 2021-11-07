e421705f58d33bf525fa6a4d989c500ee319d2da
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-11-07 12:01:51 -0300

    feat(routes): adicionado verificação de nível de acesso no componente de privateroute

--- src/components/routes/PrivateRoute/index.tsx
+++ src/components/routes/PrivateRoute/index.tsx
@@ -1,20 +1,46 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 
 import { UserContext } from "contexts/UserContext/UserContext";
 
 import { Route, Redirect } from "react-router-dom";
 
-function PrivateRoute({ component: Component, ...rest }: any) {
-    const { authenticated } = useContext(UserContext);
+type Props = {
+    component: any;
+    requiredRoles: Array<string>;
+    bypassRole?: boolean;
+    path: string;
+}
+
+const PrivateRoute: React.FC<Props> = props => {
+    const { user, authenticated } = useContext(UserContext);
+
+    const { component: Component, ...rest } = props;
+
+    if (props.bypassRole) {
+        return (
+            <Route 
+                {...rest}
+                render={(props: any) => (
+                    authenticated ? 
+                        <Component {...props} />
+                    : <Redirect to="/" />
+                )}
+            />
+        )
+    }
+
+    const userHasPermission = user.role && props.requiredRoles.includes(user.role);
 
     return (
         <Route 
             {...rest}
 
             render={(props: any) => (
-                authenticated ? 
-                    <Component {...props} /> 
-                : <Redirect to="/" /> 
+                (authenticated && userHasPermission) ?
+                    <Component {...props} />
+                : (authenticated && !userHasPermission) ?
+                    <Redirect to="/profile" />
+                : <Redirect to="/" />
             )}
         /> 
     )
--- src/routes.tsx
+++ src/routes.tsx
@@ -14,18 +14,51 @@ import { OrderRoute } from "components/routes/OrderRoute";
 import { UserDashboard } from "pages/Users/UserDashboard";
 import { PrivateRoute } from "components/routes/PrivateRoute";
 import { OrderHistory } from "pages/Orders/OrderHistory";
+import { Profile } from "pages/Profile";
 
 function Routes() {
     return (
         <BrowserRouter>
-            <PrivateRoute component={UserDashboard} path="/users" />
-            <PrivateRoute component={ OrderHistory } path="/order-history" />
-            <PrivateRoute component={ OrderBoard } path="/order-board" />
-            <OrderRoute component={ OrderRegister } path="/order" />
-            <PrivateRoute component={ OrderDashboard } path="/order-dashboard" />
-
-            <PrivateRoute component={ IngredientDashboard } path="/ingredients" />
-            <PrivateRoute component={ ProductDashboard } path="/products" />
+            <PrivateRoute 
+                requiredRoles={["admin"]}
+                component={UserDashboard} path="/users" 
+            />
+            <PrivateRoute 
+                requiredRoles={["admin", "atendente"]}
+                component={ OrderHistory }
+                path="/order-history" 
+            />
+            <PrivateRoute 
+                requiredRoles={["admin", "atendente"]}
+                component={ OrderBoard } 
+                path="/order-board" 
+            />
+            <PrivateRoute 
+                requiredRoles={["admin", "atendente"]}
+                component={ OrderDashboard } 
+                path="/order-dashboard" 
+            />
+            <PrivateRoute 
+                requiredRoles={["admin", "estoquista"]}
+                component={ IngredientDashboard } 
+                path="/ingredients" 
+            />
+            <PrivateRoute 
+                requiredRoles={["admin", "estoquista"]}
+                component={ ProductDashboard } 
+                path="/products" 
+            />
+            <OrderRoute 
+                requiredRoles={["admin", "atendente"]}
+                component={ OrderRegister } 
+                path="/order" 
+            />
+            <PrivateRoute 
+                component={ Profile }
+                requiredRoles={[]}
+                path="/profile"
+                bypassRole
+            />
             <Route component={ Login } path="/" exact/>
         </BrowserRouter>
     )
