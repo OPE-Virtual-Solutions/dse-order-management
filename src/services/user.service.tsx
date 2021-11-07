bb2664b44a4bda93d8833f0b7f21c668f0be8a37
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-11-07 10:24:02 -0300

    feat(service/user): adicionado parâmetro para filtragem de tipo de usuário

--- src/services/user.service.tsx
+++ src/services/user.service.tsx
@@ -2,6 +2,8 @@ import { api } from "api";
 
 import { User, UserPT } from "interfaces/User";
 
+type UserTypeParam = "funcionario" | "cliente";
+
 class _UserService {
     private translateListResponse(response: UserPT[]) {
         return response.map((usuario: UserPT) => {
@@ -9,8 +11,12 @@ class _UserService {
         });
     }
 
-    async list() {
-        const response = await api.get("/usuarios/");
+    async list(type: UserTypeParam | undefined = undefined) {
+        const response = await api.get("/usuarios/", {
+            params: {
+                tipo: type
+            }
+        });
 
         let list: User[] = [];
         
