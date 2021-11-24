a4a7b78740d2f57ce867d8111d6d8738fcbda2ba
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-11-24 20:41:36 -0300

    feat(page/home): adicionado novo layout da página inicial

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,12 +1,39 @@
+import { Dialog } from "@material-ui/core";
+import { UserEdit } from "components/cases/Users/UserEdit";
+import { useState } from "react";
 import { Dashboard } from "templates/Dashboard";
 
 import styles from "./Profile.module.css";
 
 function Profile() {
+    const [openModal, setOpenModal] = useState<boolean>(false);
+
+    function onEditPress() {
+        setOpenModal(true);
+    };
+
+    function handleModalClose() {
+        setOpenModal(false);
+    };
+
     return (
         <Dashboard>
             <div className={styles.container}>
+                <h3>Seja bem-vindo!</h3>
+
+                <div className={styles.nav}>
+                    <button 
+                        onClick={() => { onEditPress() }}
+                        className={styles.link}
+                    >
+                        Editar perfil
+                    </button>
+                </div>
             </div>
+
+            <Dialog fullWidth maxWidth="sm" open={openModal} onClose={() => { handleModalClose() }}>
+                <UserEdit />
+            </Dialog>
         </Dashboard>
     )
 }
